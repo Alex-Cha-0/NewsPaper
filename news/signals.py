ed555ebfb84d0dd23fd19f53161c5ee8b08b7132
Alex Chavlytko <spb.roout@gmail.com>
2023-09-08 13:40:22 +0300

    Добавлена расслыка только что созданных новостей!

--- news/signals.py
+++ news/signals.py
@@ -8,34 +8,34 @@ from django.template.loader import render_to_string
 from .models import Post, Category
 
 
-# @receiver(m2m_changed, sender=Post.category.through)
-# def update_post(sender, **kwargs):
-#     instance = kwargs.pop('instance', None)
-#     categories = kwargs['pk_set']  # Категории добавленного поста
-#     if kwargs.get('action') == 'post_add':
-#         # получем наш html
-#         html_content = render_to_string(
-#             'news/subs_users.html',
-#             {
-#                 'data': instance,
-#             }
-#         )
-#         for value in categories:
-#             category = Category.objects.get(id=value)  # Обьект Категории
-#             subscribers = category.subscribers.all()  # Подписчики одной категории
-#             for data in subscribers:
-#                 msg = EmailMultiAlternatives(
-#                     subject=f'Здравствуй, {data.username} Новая статья в твоём любимом разделе!»',
-#                     body=str(instance.text),
-#                     from_email='alexei.chavlitko@yandex.ru',
-#                     to=[data.email]
-#                 )
-#                 print('Сообщение сформировано')
-#                 msg.attach_alternative(html_content, "text/html")  # добавляем html
-#                 print('Добавлен html')
-#                 msg.send()
-#                 print('Сообщение отправлено')
-#                 print('--------------')
+@receiver(m2m_changed, sender=Post.category.through)
+def update_post(sender, **kwargs):
+    instance = kwargs.pop('instance', None)
+    categories = kwargs['pk_set']  # Категории добавленного поста
+    if kwargs.get('action') == 'post_add':
+        # получем наш html
+        html_content = render_to_string(
+            'news/subs_users.html',
+            {
+                'data': instance,
+            }
+        )
+        for value in categories:
+            category = Category.objects.get(id=value)  # Обьект Категории
+            subscribers = category.subscribers.all()  # Подписчики одной категории
+            for data in subscribers:
+                msg = EmailMultiAlternatives(
+                    subject=f'Здравствуй, {data.username} Новая статья в твоём любимом разделе!»',
+                    body=str(instance.text),
+                    from_email='alexei.chavlitko@yandex.ru',
+                    to=[data.email]
+                )
+                print('Сообщение сформировано')
+                msg.attach_alternative(html_content, "text/html")  # добавляем html
+                print('Добавлен html')
+                msg.send()
+                print('Сообщение отправлено')
+                print('--------------')
 
 @receiver(email_confirmed)
 def user_signed_up_(request, email_address, **kwargs):
